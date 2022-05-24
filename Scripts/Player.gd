fec19533f8ef4838d59eb80471c39c11cdb2eb52
alexcstd <alex.scustod@gmail.com>
2022-05-23 22:18:17 -0300

    player new movements added

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -8,31 +8,51 @@ const gravity = 20
 const acceleration = 20
 const max_speed = 130
 const jump_height = -400
+var flipped = false
+
+
 
 func _physics_process(delta):
+	flipped = false
 	motion.y += gravity
 	var friction = false
 	if Input.is_action_pressed("ui_right"):
+		flipped = false
 		motion.x = min(motion.x+acceleration, max_speed)
+		$AnimatedSprite.play("walk-right")
 		
 	elif Input.is_action_pressed("ui_left"):
+		flipped = true
 		motion.x = max(motion.x-acceleration, -max_speed)
+		$AnimatedSprite.play("walk-left")
+		
 		
 	else:
+		if flipped == true:
+			$AnimatedSprite.flip_h = true
+		elif flipped == false:
+			$AnimatedSprite.flip_h = false
 		friction = true
 		$AnimatedSprite.play("idle")
 		
+	if Input.is_action_pressed("ui_accept"):
+		$AnimatedSprite.play("attack")
 	if is_on_floor():
 		if Input.is_action_just_pressed("ui_up"):
 			motion.y = jump_height
 		if friction == true:
 			motion.x = lerp(motion.x, 0,0.2)
 	else:
-		#pra quando for fazer animação
-		#if motion.y < 0:
-			#$AnimatedSprite.play("jump")
-		#else:
-			#$AnimatedSprite.play("fall")
+		
+		if motion.y < 0:
+			if flipped == true:
+				$AnimatedSprite.flip_h = true
+			$AnimatedSprite.play("jump")
+			
+		else:
+			$AnimatedSprite.play("fall")
+			
+			
 		if friction == true:
 			motion.x = lerp(motion.x, 0,0.05)
 	motion = move_and_slide(motion, up)
